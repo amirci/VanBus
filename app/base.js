54eb7bc6933e63f06e8846ea05ab851a3f6cece1
Chris Nicola <chrisn@tictalking.com>
2012-05-27 10:54:06 -0700

    Initial checkin of the VanBus on Mulberry

---
+++ app/base.js
@@ -0,0 +1,6 @@
+/**
+ * You can write custom JavaScript here; this is also where Mulberry will add
+ * any dependencies you introduce via `mulberry create component`.
+ */
+dojo.require('toura.base');
+dojo.require('client.routes');
